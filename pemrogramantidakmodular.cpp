d64dc0b9592878a6144fe8f49aeef49c07c772c2
Santi18 <santinoviani97@gmail.com>
2017-03-21 20:34:26 +0700

    Add files via upload

---
+++ pemrogramantidakmodular.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	//variabel
+	int a, b, temp;
+	
+	//proses baca
+	cout << "Masukan nilai A : ";
+	cin >> a;
+	
+	cout << "Masukan nilai B : ";
+	cin >> b;
+	
+	//proses tukar
+	temp = b;
+	b = a;
+	a = temp;
+	
+	//proses tampil
+	cout << "===================================" << endl;
+	cout << "Nilai A sekarang : " << a << endl;
+	cout << "Nilai B sekarang : " << b;
+	
+	return 0;
+	}
